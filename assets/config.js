50dfdb7d4305479ae365536165a9cefe36656e83
pareshsarkar0908-dotcom <pareshsarkar0908@gmail.com>
2026-06-01 00:54:13 +0530

    Delete assets directory

--- assets/config.js
+++
@@ -1,3 +0,0 @@
-window.SALESMART_SUPABASE_URL = 'https://dkrjwqajnyfcfqdiswda.supabase.co';
-window.SALESMART_SUPABASE_ANON_KEY = 'sb_publishable_2cHKu0mts259ARFiC1T7DQ_ZQZv1duR';
-window.SALESMART_RAZORPAY_KEY_ID = 'rzp_live_SvxaS8NNrTTmVP';
