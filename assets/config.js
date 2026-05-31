1d73fac515adc93f02e7d1965347c6ae5cc9d47a
pareshsarkar0908-dotcom <pareshsarkar0908@gmail.com>
2026-05-31 18:48:03 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
-window.SALESMART_SUPABASE_URL = 'https://your-project.supabase.co';
-window.SALESMART_SUPABASE_ANON_KEY = 'sb_publishable_your_key_here';
-window.SALESMART_RAZORPAY_KEY_ID = 'rzp_test_or_live_key_id';
+window.SALESMART_SUPABASE_URL = 'https://dkrjwqajnyfcfqdiswda.supabase.co';
+window.SALESMART_SUPABASE_ANON_KEY = 'sb_publishable_2cHKu0mts259ARFiC1T7DQ_ZQZv1duR';
+window.SALESMART_RAZORPAY_KEY_ID = 'rzp_live_SvxaS8NNrTTmVP';
