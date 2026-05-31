603b80cca540cbe353dcadc414289b632cbe1015
pareshsarkar0908-dotcom <pareshsarkar0908@gmail.com>
2026-05-31 18:40:29 +0530

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,3 @@
+window.SALESMART_SUPABASE_URL = 'https://your-project.supabase.co';
+window.SALESMART_SUPABASE_ANON_KEY = 'sb_publishable_your_key_here';
+window.SALESMART_RAZORPAY_KEY_ID = 'rzp_test_or_live_key_id';
